cbda589296401410ab051be3d824c0e28e9946b5
李柏誼 <91170940+lbyNevaeh@users.noreply.github.com>
2023-03-02 16:31:38 +0800

    Add files via upload

---
+++ week03/week03-1_translate/main.cpp
@@ -0,0 +1,17 @@
+#include <GL/glut.h>
+void display()
+{
+    glPushMatrix();///備份矩陣
+        glTranslatef(0.5,0.5,0);///會改變你的矩陣
+        glutSolidTeapot( 0.3 );
+    glPopMatrix();///還原矩陣
+	glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("GLUT Shapes");
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
